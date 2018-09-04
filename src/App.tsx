c638e7372d22b42aedb006829c55a0f6303ca4ee
rdeles <riccideles@gmail.com>
2018-09-04 14:37:27 +1200

    Setting up base project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
+      <div className="container-fluid">
+      <div className="centreText">
+        {/* React components must have a wrapper node/element */}
+        <h1>( ͡° ͜ʖ ͡°)</h1>
+      </div>
+      </div>
+      /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -14,7 +18,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-      </div>
+      </div>*/
     );
   }
 }
